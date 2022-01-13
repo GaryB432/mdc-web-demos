4542e404747be2476e358ca3cf48d101dd9451b9
Gary Bortosky <garybort@gmail.com>
2022-01-13 15:34:52 -0600

    initial commit

---
+++ apps/adopt-a-pup/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'adopt-a-pup',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/adopt-a-pup',
+};
